877bc9c761a8a0256de72f5dece8508f7f178541
aggelinho <georgeaggelis74@gmail.com>
2026-05-15 19:47:01 +0300

    Αφαιρέσαμε τα κατάλοιπα του Figma, βελτίωση του οπτικού κομματιού και ανάπτυξη backend δομής. Αναπτύχθηκε σε συνεργασία με Σωτήρη Γιαννακόπουλο και Γιώργο Πετρόπουλο.

--- Code/src/main.tsx
+++ Code/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 // @ts-ignore
 import { createRoot } from 'react-dom/client'
+import './styles/index.css'
 import App from './app/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
