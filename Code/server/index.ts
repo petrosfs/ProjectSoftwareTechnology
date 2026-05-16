6bb66a181eaa91fcfe5d87ed8b79a6b2e566cdba
aggelinho <georgeaggelis74@gmail.com>
2026-05-16 21:56:59 +0300

    fix: update SPA catch-all route to Express 5 wildcard syntax

--- Code/server/index.ts
+++ Code/server/index.ts
@@ -37,7 +37,7 @@ app.use('/api', apiRouter);
 if (process.env.NODE_ENV === 'production') {
   const distPath = path.resolve(__dirname, '../dist');
   app.use(express.static(distPath));
-  app.get('*', (_req, res) => {
+  app.get('{*path}', (_req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
